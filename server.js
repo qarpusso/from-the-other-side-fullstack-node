1414e77c37c698ff2ca9bf1e6260bc921553f9d3
qarpusso@gmail.com <qarpusso@gmail.com>
2026-02-23 21:56:28 +0300

    checkpoint

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
-import http from "node:http"
-import fs from "node:fs"
-
-import { serveStatic } from "./utils/serveStatic.js"
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
+
 const __dirname = import.meta.dirname
 
-const server = http.createServer((req, res) => {
-  const urlObj = new URL(req.url, `http://${req.headers.host}`)
-  const content = fs.readFile(serveStatic())
+const server = http.createServer(async (req, res) => {
+    await serveStatic(__dirname)
 
-  if (urlObj.pathname === "/api") {
-    const data = { ok: true }
-    const json = JSON.stringify(data)
-    res.writeHead(200, {
-      "Content-Type": "application/json charset=utf-8",
-      "Content-Length": Buffer.byteLength(json),
-    })
-    res.end(json)
-    return
-  }
-
-  res.writeHead(200, {
-    "Content-Type": "text/html charset=utf-8",
-    "Content-Length": Buffer.byteLength(content),
-  })
-  res.end(content)
 })
 
-server.listen(PORT, () => console.log("Server started at PORT:", PORT))
+server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,7 +1,16 @@
 import path from 'node:path'
+import fs from 'node:fs/promises'
 
-export function serveStatic(baseDir) {
+import { sendResponse } from './sendResponse.js'
+
+export async function serveStatic(req, res, baseDir) {
+  
   const filePath = path.join(baseDir, 'public', 'index.html')
-  console.log(filePath)
-  return filePath
+
+  try {
+    const content = await fs.readFile(filePath)
+    sendResponse(res, 200, "text/html", content)
+  } catch (err) {
+    console.log(err)
+  }
 }
